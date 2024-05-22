842ba59edd8ea9f3dd54f87bdbd295b2f79a8ca6
Jbugden97 <james.bugden97@gmail.com>
2024-05-22 19:58:09 +1000

    add copy functionality

--- src/app/components/output-codeblock.tsx
+++ src/app/components/output-codeblock.tsx
@@ -1,20 +1,30 @@
 "use-client";
 
+import { useState } from "react";
+
 interface OutputCodeBlockProps {
   sqlOutput: string;
 }
 
 const OutputCodeBlock: React.FC<OutputCodeBlockProps> = ({ sqlOutput }) => {
+  const [copy, setCopy] = useState<boolean>(false);
   return (
-    <div className="relative mx-auto">
+    <div className="relative mx-auto mb-64">
       <div className="bg-gray-900 text-white p-4 rounded-md">
         <div className="flex justify-between items-center mb-2">
           <span className="text-gray-400 text-lg">Code:</span>
           <button
             className="code bg-gray-800 hover:bg-gray-700 text-gray-300 px-3 py-1 rounded-md text-lg"
             data-clipboard-target="#code"
+            onClick={async () => {
+              await navigator.clipboard.writeText(sqlOutput);
+              setCopy(true);
+              setTimeout(() => {
+                setCopy(false);
+              }, 1000);
+            }}
           >
-            Copy
+            {copy ? "Copied!" : "Copy"}
           </button>
         </div>
         <div className="overflow-x-auto text-sm">
